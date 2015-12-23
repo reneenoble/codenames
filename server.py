72233e6732d64a75de381b5288368db0330263e7
reneenoble <rnob8647@uni.sydney.edu.au>
2015-12-23 22:22:19 +1100

    made a database that stores a list of names
    prints the names on the index page

--- server.py
+++ server.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
+import sqlite3
 from tornado.ncss import Server
 
 indexhtml = open("index.html").read()
 
+conn = sqlite3.connect('codenames.db', isolation_level=None)
+
+def create_players_table():
+	conn.execute("""CREATE TABLE IF NOT EXISTS players (
+		name TEXT PRIMARY KEY
+	)""")
+
+def get_names():
+	cursor = conn.execute("SELECT * FROM players")
+	tuples = cursor.fetchall()
+	names = [name for name, in tuples]
+	return names
+
+
 def index(response):
-	response.write(indexhtml)
+	response.write(indexhtml.format(", ".join(get_names())))
 
 def gamepage(response, name):
 	response.write("So you like to play " + name + "?")
+
 
 server = Server()
 server.register("/", index)
 server.register("/game/([A-Z][a-z]*)", gamepage)
 
+create_players_table()
 server.run()
