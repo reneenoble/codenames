805700bbc86e647b7a94fc4905c14a0a7f50f479
reneenoble <rnob8647@uni.sydney.edu.au>
2016-01-02 11:14:46 +1100

    changed tabs to spaces

--- db.py
+++ db.py
@@ -4,139 +4,139 @@ import sqlite3
 conn = sqlite3.connect('codenames.db', isolation_level=None)
 
 def init():
-	create_players_table()
-	create_games_table()
-	create_game_players_table()
-	create_words_table()
-	create_game_words_table()
+  create_players_table()
+  create_games_table()
+  create_game_players_table()
+  create_words_table()
+  create_game_words_table()
 
 def create_players_table():
-	conn.execute("""CREATE TABLE IF NOT EXISTS players (
-		player_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT UNIQUE,
-		display_name TEXT, 
-		password TEXT
-	)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS players (
+    player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    display_name TEXT, 
+    password TEXT
+  )""")
 
 def create_games_table ():
-	conn.execute("""CREATE TABLE IF NOT EXISTS games (
-		game_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		room_code CHAR(4),
-		game_state TEXT
-		)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS games (
+    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    room_code CHAR(4),
+    game_state TEXT
+    )""")
 
 def create_game_players_table():
-	conn.execute("""CREATE TABLE IF NOT EXISTS game_players (
-		game_id INTEGER,
-		player_id INTEGER,
-		team TEXT,
-		spymaster BOOLEAN,
-		PRIMARY KEY (game_id, player_id),
-		FOREIGN KEY (game_id) REFERENCES games(game_id),
-		FOREIGN KEY (player_id) REFERENCES players(player_id)
-		)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS game_players (
+    game_id INTEGER,
+    player_id INTEGER,
+    team TEXT,
+    spymaster BOOLEAN,
+    PRIMARY KEY (game_id, player_id),
+    FOREIGN KEY (game_id) REFERENCES games(game_id),
+    FOREIGN KEY (player_id) REFERENCES players(player_id)
+    )""")
 
 def create_words_table():
-	conn.execute("""CREATE TABLE IF NOT EXISTS words (
-		word TEXT PRIMARY KEY
-		)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS words (
+    word TEXT PRIMARY KEY
+    )""")
 
 def create_game_words_table():
-	conn.execute("""CREATE TABLE IF NOT EXISTS game_words (
-		game_id INTEGER, 
-		word TEXT,
-		colour TEXT,
-		position INTEGER,
-		guessed BOOLEAN,
-		FOREIGN KEY (game_id) REFERENCES games(game_id),
-		FOREIGN KEY (word) REFERENCES words(word)
-		)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS game_words (
+    game_id INTEGER, 
+    word TEXT,
+    colour TEXT,
+    position INTEGER,
+    guessed BOOLEAN,
+    FOREIGN KEY (game_id) REFERENCES games(game_id),
+    FOREIGN KEY (word) REFERENCES words(word)
+    )""")
 
 def create_clues_table():
-	conn.execute("""CREATE TABLE IF NOT EXISTS clues (
-		clue_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		game_id INTEGER,
-		clue TEXT,
-		clue_maker INTEGER,
-		FOREIGN KEY (clue_maker) REFERENCES players(player_id)
-		)""")
+  conn.execute("""CREATE TABLE IF NOT EXISTS clues (
+    clue_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER,
+    clue TEXT,
+    clue_maker INTEGER,
+    FOREIGN KEY (clue_maker) REFERENCES players(player_id)
+    )""")
 
 def get_names():
-	cursor = conn.execute("SELECT * FROM players")
-	tuples = cursor.fetchall()
-	names = [username for player_id, username, disp_name, pword in tuples]
-	return names
+  cursor = conn.execute("SELECT * FROM players")
+  tuples = cursor.fetchall()
+  names = [username for player_id, username, disp_name, pword in tuples]
+  return names
 
 def add_player(name):
-	cursor = conn.execute("INSERT INTO players (username) VALUES (?)", (name,))
-	player_id = cursor.lastrowid
-	return player_id
+  cursor = conn.execute("INSERT INTO players (username) VALUES (?)", (name,))
+  player_id = cursor.lastrowid
+  return player_id
 
 def user_exists(username):
-	cursor = conn.execute("SELECT * FROM players WHERE username = ?", (username,))
-	users = cursor.fetchall()
-	if users:
-		return users[0][0]
-	else:
-		return None
+  cursor = conn.execute("SELECT * FROM players WHERE username = ?", (username,))
+  users = cursor.fetchall()
+  if users:
+    return users[0][0]
+  else:
+    return None
 
 def create_game(code):
-	cursor = conn.execute("INSERT INTO games (room_code, game_state) VALUES (?, ?)", (code, "lobby"))
-	game_id = cursor.lastrowid
-	return game_id
+  cursor = conn.execute("INSERT INTO games (room_code, game_state) VALUES (?, ?)", (code, "lobby"))
+  game_id = cursor.lastrowid
+  return game_id
 
 def room_code_in_use(code):
-	cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
-	games = cursor.fetchall()
-	if games:
-		return True
-	else:
-		return False
+  cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
+  games = cursor.fetchall()
+  if games:
+    return True
+  else:
+    return False
 
 def get_game_state(code):
-	cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
-	games = cursor.fetchall()
-	if games:
-		state = games[0][2]
-		return state
-	else:
-		return("not active")
+  cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
+  games = cursor.fetchall()
+  if games:
+    state = games[0][2]
+    return state
+  else:
+    return("not active")
 
 def get_active_game_id(code):
-	cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
-	games = cursor.fetchall()
-	if games:
-		game_id = games[0][0]
-		return game_id
-	else:
-		return None
+  cursor = conn.execute("SELECT * FROM games WHERE room_code = ? AND game_state != ? ", (code, "finished"))
+  games = cursor.fetchall()
+  if games:
+    game_id = games[0][0]
+    return game_id
+  else:
+    return None
 
 def add_game_player(game_id, player_id):
-	conn.execute("INSERT INTO game_players (game_id, player_id) VALUES (?, ?)", (game_id, player_id))
+  conn.execute("INSERT INTO game_players (game_id, player_id) VALUES (?, ?)", (game_id, player_id))
 
 def get_player_id(username):
-	cursor = conn.execute("SELECT * FROM players WHERE username = ?", (username,))
-	players = cursor.fetchall()
-	if players:
-		user_id = players[0][0]
-		return user_id
-	else:
-		return None
+  cursor = conn.execute("SELECT * FROM players WHERE username = ?", (username,))
+  players = cursor.fetchall()
+  if players:
+    user_id = players[0][0]
+    return user_id
+  else:
+    return None
 
 def get_game_players(code):
-	game_id = get_active_game_id(code)
-	cursor = conn.execute("SELECT * FROM game_players WHERE game_id = ?", (game_id,))
-	game_instances = cursor.fetchall()
-	game_players = [conn.execute("SELECT * FROM players where player_id = ?", (i[1],)).fetchall()[0][1] for i in game_instances]
-	return game_players
+  game_id = get_active_game_id(code)
+  cursor = conn.execute("SELECT * FROM game_players WHERE game_id = ?", (game_id,))
+  game_instances = cursor.fetchall()
+  game_players = [conn.execute("SELECT * FROM players where player_id = ?", (i[1],)).fetchall()[0][1] for i in game_instances]
+  return game_players
 
 def player_in_game(player_id, code):
-	game_id = get_active_game_id(code)
-	cursor = conn.execute("SELECT * FROM game_players WHERE game_id = ? AND player_id = ?", (game_id, player_id))
-	players = cursor.fetchall()
-	if players:
-		return True
-	else:
-		return False
+  game_id = get_active_game_id(code)
+  cursor = conn.execute("SELECT * FROM game_players WHERE game_id = ? AND player_id = ?", (game_id, player_id))
+  players = cursor.fetchall()
+  if players:
+    return True
+  else:
+    return False
 
 
--- server.py
+++ server.py
@@ -9,120 +9,121 @@ loader = Loader(".")
 #loader = Loader("./templates")
 
 def index(response):
-	indexhtml = loader.load("index.html")
-	response.write(indexhtml.generate(names=db.get_names()))
+  indexhtml = loader.load("index.html")
+  response.write(indexhtml.generate(names=db.get_names()))
 
 def register(response):
-	registerhtml = loader.load("register.html")
-	response.write(registerhtml)
+  registerhtml = loader.load("register.html")
+  response.write(registerhtml)
 
 def game(response):
-	gamehtml = loader.load("game.html")
-	response.write(gamehtml.generate(username=get_user(response)))
+  gamehtml = loader.load("game.html")
+  response.write(gamehtml.generate(username=get_user(response)))
 
 def lobby(response, roomcode):
-	in_game = player_in_game(response, roomcode)
-	print(in_game)
-	if in_game:
-		lobbyhtml = loader.load("lobby.html")
-		response.write(lobbyhtml.generate(code=roomcode, game_players=db.get_game_players(roomcode)))
-	else:
-		response.redirect("/game")
+  in_game = player_in_game(response, roomcode)
+  print(in_game)
+  if in_game:
+    lobbyhtml = loader.load("lobby.html")
+    response.write(lobbyhtml.generate(code=roomcode, game_players=db.get_game_players(roomcode)))
+  else:
+    response.redirect("/game")
 
 def game_page(response, name):
-	response.write("So you like to play " + name + "?")
+  response.write("So you like to play " + name + "?")
 
 def add_name_page(response):
-	name = response.get_field("username")
-	db.add_player(name)
-	response.redirect("/")
+  name = response.get_field("username")
+  db.add_player(name)
+  response.redirect("/")
 
 def login_page(response):
-	username = response.get_field("username")
-	user_exists = db.user_exists(username)
-	if user_exists:
-		player_id = db.get_player_id(username)
-		login(response, username, player_id)
-	else:
-		response.redirect("/")
+  username = response.get_field("username")
+  user_exists = db.user_exists(username)
+  if user_exists:
+    player_id = db.get_player_id(username)
+    login(response, username, player_id)
+  else:
+    response.redirect("/")
 
 def login(response, username, player_id):
-	response.set_secure_cookie("username", username) 
-	response.set_secure_cookie("player_id", str(player_id))
-	response.redirect("/game")
+  response.set_secure_cookie("username", username) 
+  response.set_secure_cookie("player_id", str(player_id))
+  response.redirect("/game")
 
 def register_user(response):
-	username = response.get_field("username")
-	if not db.user_exists(username):
-		player_id = db.add_player(username)
-		login(response, username, player_id)
+  username = response.get_field("username")
+  if not db.user_exists(username):
+    player_id = db.add_player(username)
+    login(response, username, player_id)
 
 def get_user(response):
-	username = response.get_secure_cookie("username")
-	return username
+  username = response.get_secure_cookie("username")
+  return username
 
 def create_game(response):
-	code_in_use = True
-	while code_in_use:
-		#create a roomcode, 
-		code = randomword(4)
-		#check if code is in use
-		code_in_use = db.room_code_in_use(code)
-	#add it to the database of room codes
-	game_id = db.create_game(code)
-	#add the player to the games_players table for this room redirect to the lobby for the room code
-	join_lobby(response, code)
+  code_in_use = True
+  while code_in_use:
+    #create a roomcode, 
+    code = randomword(4)
+    #check if code is in use
+    code_in_use = db.room_code_in_use(code)
+  #add it to the database of room codes
+  game_id = db.create_game(code)
+  #add the player to the games_players table for this room redirect to the lobby for the room code
+  join_lobby(response, code)
 
 def join_game(response):
-	code = response.get_field("roomcode")
-	join_lobby(response, code)
+  code = response.get_field("roomcode")
+  join_lobby(response, code)
 
 def join_lobby(response, roomcode):
-	#check if room code exists and what it's status is
-	state = db.get_game_state(roomcode)
-	if state == "lobby":
-		game_id = db.get_active_game_id(roomcode)
-		#add player to game players table
-		player_id = get_player_id_cookies(response)
-		db.add_game_player(game_id, player_id)
-		#redirect to lobby page
-		response.redirect("/lobby/" + roomcode)
-	elif state == "playing":
-		#The game in this room has already started. 
-		print("The game in this room has already started. ")
-	else:
-		#You must either have the wring room code or the game you want has already finished
-		print("You must either have the wrong room code or the game you want has already finished")
+  #check if room code exists and what it's status is
+  state = db.get_game_state(roomcode)
+  if state == "lobby":
+    game_id = db.get_active_game_id(roomcode)
+    #add player to game players table
+    player_id = get_player_id_cookies(response)
+    db.add_game_player(game_id, player_id)
+    #redirect to lobby page
+    response.redirect("/lobby/" + roomcode)
+  elif state == "playing":
+    #The game in this room has already started. 
+    print("The game in this room has already started. ")
+  else:
+    #You must either have the wring room code or the game you want has already finished
+    print("You must either have the wrong room code or the game you want has already finished")
 
 def start_game(response, roomcode):
-	#If player in game
-	if player_in_game(response, roomcode):
-	#If game already started go to the game board
-		if game_started(roomcode):
+  #If player in game
+  if player_in_game(response, roomcode):
+  #If game already started go to the game board
+    if game_started(roomcode):
 
-			#Otherwise start game
-			#change game state to playing
-			
-			#assign players randomly to teams and to spymaster
-			assign_teams_and_roles(roomcode)
-			#choose words for the game
-			#redirect to the game board
-		else:
-			response.redirect("game/play/roomcode")
+      #Otherwise start game
+      #change game state to playing
+      
+      #assign players randomly to teams and to spymaster
+      assign_teams_and_roles(roomcode)
+      #choose words for the game
+      #redirect to the game board
+    else:
+      response.redirect("game/play/roomcode")
 
-	else:
-		response.redirect("/game")
+  else:
+    response.redirect("/game")
 
 def randomword(length):
-	return ''.join(random.choice("abcdefghijklmnopqrstuvwxyz") for i in range(length))
+  return ''.join(random.choice("abcdefghijklmnopqrstuvwxyz") for i in range(length))
 
 def get_player_id_cookies(response):
-	return int(response.get_secure_cookie("player_id"))
+  return int(response.get_secure_cookie("player_id"))
 
 def player_in_game(response, roomcode):
-	player_id = get_player_id_cookies(response)
-	in_game = db.player_in_game(player_id, roomcode)
-	return in_game
+  player_id = get_player_id_cookies(response)
+  in_game = db.player_in_game(player_id, roomcode)
+  return in_game
+
 
 server = Server()
 #server.register("/", index, post=add_name_page)
